1cc80b050fb31eb3d171d65cc4c404ce5258d0c8
not_nimo <damiano.livoni@gmail.com>
2026-05-24 23:24:01 +0100

    dev: added session confirm messages

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import sys
 
 from src.cli.argsHandle import handleArgs
+from src.const.driversList import getDriverWithId
 
 def main(driver1, driver2=-1, year=-1, round_number=-1, session_type='R'):
-  print("session confirm message") # @TODO TO CHANGE
+  if driver2 == None:
+    driver2 = -1
+  if year == None:
+    year = -1
+  if round_number == None:
+    round_number = -1
+  if session_type == None:
+    session_type = 'R'
+  driver1_name = getDriverWithId(int(driver1))
+  driver2_name = getDriverWithId(int(driver2)) if driver2 != -1 else None
+  print(f"session confirmed for {driver1_name} {f'and {driver2_name}' if driver2_name else '\0'}{f' in ' if year != -1 or round_number != -1 else '\0'}{f'{year}' if year != -1 else '\0'}{f' round {round_number}' if round_number != -1 else '\0'}({session_type})") # @TODO TO CHANGE
   # validate args
 
   # enable cache
