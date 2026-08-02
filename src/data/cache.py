f49bd97f69e7ef38bb590b12c9ba9c53feb911cc
notnimo <damiano.livoni@gmail.com>
2026-08-02 22:57:50 +0200

    dev: added cache managment

---
+++ src/data/cache.py
@@ -0,0 +1,17 @@
+import os
+import fastf1
+
+from lib.settings import get_settings
+
+
+def enable_cache():
+    # Get cache location from settings
+    settings = get_settings()
+    cache_path = settings.cache_location
+
+    # Check if cache folder exists
+    if not os.path.exists(cache_path):
+        os.makedirs(cache_path)
+
+    # Enable local cache
+    fastf1.Cache.enable_cache(cache_path)
